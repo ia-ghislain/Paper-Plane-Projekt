1612e55994512e065fd45e17355f4f334f524a00
ghighi-du63000 <ghighi-du63000@users.noreply.github.com>
2015-01-23 00:42:21 +0100

    Sample menu added
     > Transformed the game into an object
     > Added function show menu (not working proprely yet)
     > Added main function triggered @ start
     > UGLY !!!

--- Source/paper_plane.py
+++ Source/paper_plane.py
@@ -1,4 +1,5 @@
 import pygame, random
+from menu import *
 from time import *
 '''
 Global constants
@@ -119,7 +120,101 @@ class Wall(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.y = y
 		self.rect.x = x
-	
+
+class Menu(object):
+	legacy_list = []
+	fields = []
+	font_size = 32
+	font_path = 'data/coders_crux.ttf'
+	font = pygame.font.Font
+	dest_surface = pygame.Surface
+	fields_quantity = 0
+	background_color = (51, 51, 51)
+	text_color = (255, 255, 153)
+	selection_color = (153, 102, 255)
+	selection_position = 0
+	paste_position = (0, 0)
+	menu_width = 0
+	menu_height = 0
+
+	class Pole(object):
+		text = ''
+		field = pygame.Surface
+		field_rect = pygame.Rect
+		selection_rect = pygame.Rect
+
+	def move_menu(self, top, left):
+		self.paste_position = (top, left)
+
+	def set_colors(self, text, selection, background):
+		self.background_color = background
+		self.text_color = text
+		self.selection_color = selection
+
+	def set_fontsize(self, font_size):
+		self.font_size = font_size
+
+	def set_font(self, path):
+		self.font_path = path
+
+	def get_position(self):
+		return self.selection_position
+
+	def init(self, legacy_list, dest_surface):
+		self.legacy_list = legacy_list
+		self.dest_surface = dest_surface
+		self.fields_quantity = len(self.legacy_list)
+		self.create_structure()
+
+	def draw(self, move=0):
+		if move:
+			self.selection_position += move
+			if self.selection_position == -1:
+				self.selection_position = self.fields_quantity - 1
+			self.selection_position %= self.fields_quantity
+		menu = pygame.Surface((self.menu_width, self.menu_height))
+		menu.fill(self.background_color)
+		selection_rect = self.fields[self.selection_position].zaznaczenie_rect
+		pygame.draw.rect(menu, self.selection_color, selection_rect)
+
+		for i in xrange(self.fields_quantity):
+			menu.blit(self.fields[i].pole, self.fields[i].pole_rect)
+		self.dest_surface.blit(menu, self.paste_position)
+		return self.selection_position
+
+	def create_structure(self):
+		self.menu_height = 0
+		self.font = pygame.font.Font(self.font_path, self.font_size)
+		for i in xrange(self.fields_quantity):
+			self.fields.append(self.Pole())
+			self.fields[i].tekst = self.legacy_list[i]
+			self.fields[i].pole = self.font.render(
+				self.fields[i].tekst,
+				1,
+				self.text_color
+			)
+
+			self.fields[i].pole_rect = self.fields[i].pole.get_rect()
+			move = int(self.font_size * 0.2)
+
+			height = self.fields[i].pole_rect.height
+			self.fields[i].pole_rect.left = move
+			self.fields[i].pole_rect.top = move + (move * 2 + height) * i
+
+			width = self.fields[i].pole_rect.width + move * 2
+			height = self.fields[i].pole_rect.height + move * 2
+			left = self.fields[i].pole_rect.left - move
+			top = self.fields[i].pole_rect.top - move
+
+			self.fields[i].zaznaczenie_rect = (left, top, width, height)
+			if width > self.menu_width:
+					self.menu_width = width
+			self.menu_height += height
+		x = self.dest_surface.get_rect().centerx - self.menu_width / 2
+		y = self.dest_surface.get_rect().centery - self.menu_height / 2
+		mx, my = self.paste_position
+		self.paste_position = (x+mx, y+my)
+
 def gen_wall(pos,slimit=500,color=BROWN):
 	size = random.randint(100,slimit)
 	# pos = random.randint(0,1)
@@ -135,6 +230,91 @@ def gen_wall(pos,slimit=500,color=BROWN):
 	# wall_list.add(wall)
 	# all_sprite_list.add(wall)
 
+class Play(object):
+	"""Play the game with some parameters"""
+	param = {}
+	def __init__(self,uparam):
+		#super(self.__class__, self).__init__()
+		super(Play, self).__init__()
+		dparam = {	
+					"pcolor":ORANGE,
+					"dynamic_speed":False,
+					"speed":1,
+					"tspeed":3,
+				}
+		self.param.update(dparam) #Merge given array & default array
+	def start(self):
+		frame_wall_list = pygame.sprite.Group()
+		# Left side wall
+		wall = Wall(0, 0, 10, 600,BLACK)
+		frame_wall_list.add(wall)
+		all_sprite_list.add(wall)
+		''' Obstacles samples
+		self, x, y, width, height,color=BROWN
+		'''
+		wall = Wall(10, SCREEN_HEIGHT/5, 300, 500,BLUE)
+		wall_list.add(wall)
+		all_sprite_list.add(wall)
+		
+		wall = Wall(SCREEN_WIDTH-310, SCREEN_HEIGHT/5, 300, 500,BLUE)
+		wall_list.add(wall)
+		all_sprite_list.add(wall)
+		
+		''' End of Obstacles '''
+		
+		# Right side wall
+		wall = Wall(SCREEN_WIDTH-10, 0, 10, 600,BLACK)
+		frame_wall_list.add(wall)
+		all_sprite_list.add(wall)
+		
+		
+		# Create the player paddle object @ the middle of the screen
+		player = Player(SCREEN_WIDTH/2, 0)
+		player.walls = wall_list
+		player.frame_walls = frame_wall_list
+		
+		all_sprite_list.add(player)
+		
+		clock = pygame.time.Clock()
+		
+		done = False
+		
+		speed = 1 # Speed of the airplane
+		tspeed = 3 # Turning speed
+		tesla,ttesla = 0,0 # Time elapsed since last action
+		player.changespeed(0,speed) # Not turning at t=0
+		dt = clock.tick(FPS) # delta of t
+		pos = POS_RIGHT # Start the game @ left position
+		while not done:
+			screen.fill(WHITE) # Clean the screen
+			tesla += dt
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					done = True
+				elif event.type == EVENT_TL:
+					player.changespeed(-tspeed, speed)
+					pygame.time.set_timer(EVENT_TL, 0) # Stop the event to be repeated
+				elif event.type == EVENT_TR:
+					player.changespeed(tspeed, speed)
+					pygame.time.set_timer(EVENT_TR, 0) # Stop the event to be repeated
+				elif event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_LEFT:
+						pygame.time.set_timer(EVENT_TL,250) #25 ms before the action is realised
+					elif event.key == pygame.K_RIGHT:
+						pygame.time.set_timer(EVENT_TR,250) #25 ms before the action is realised
+			if (tesla > 2000):
+				pos = 1-pos #Turn in the opposite dir.
+				tesla = 0 # Reset timer
+				gen_wall(pos) #Generate a wall
+				print "Time to generate wall"
+			# Rendering
+			all_sprite_list.draw(screen) # Draw everything so that text will be on top
+			all_sprite_list.update()
+			pygame.display.flip()
+			clock.tick(FPS) #Frame rate (in milliseconds)
+		pygame.quit()
+
+
 # Call this function so the Pygame library can initialize itself
 pygame.init()
 
@@ -149,74 +329,45 @@ all_sprite_list = pygame.sprite.Group()
 
 # Make the walls. (x_pos, y_pos, width, height)
 wall_list = pygame.sprite.Group()
-frame_wall_list = pygame.sprite.Group()
-
-# Left side wall
-wall = Wall(0, 0, 10, 600,BLACK)
-frame_wall_list.add(wall)
-all_sprite_list.add(wall)
-
-''' Obstacles 
-self, x, y, width, height,color=BROWN
-'''
-wall = Wall(10, SCREEN_HEIGHT/5, 300, 500,BLUE)
-wall_list.add(wall)
-all_sprite_list.add(wall)
-
-wall = Wall(SCREEN_WIDTH-310, SCREEN_HEIGHT/5, 300, 500,BLUE)
-wall_list.add(wall)
-all_sprite_list.add(wall)
-
-''' End of Obstacles '''
-
-# Right side wall
-wall = Wall(SCREEN_WIDTH-10, 0, 10, 600,BLACK)
-frame_wall_list.add(wall)
-all_sprite_list.add(wall)
-
-
-# Create the player paddle object @ the middle of the screen
 player = Player(SCREEN_WIDTH/2, 0)
-player.walls = wall_list
-player.frame_walls = frame_wall_list
+game = Play({"hey":False})
 
-all_sprite_list.add(player)
+def show_menu(menu): #Var menu is a list with (name:function)
+	screen.fill((51, 51, 51))
+	keys = menu.keys()
+	menu = Menu()
+	menu.init(keys, screen)  # necessary
+	menu.draw()
+	pygame.key.set_repeat(199, 69)  # (delay,interval)
+	pygame.display.update()
+	while 1:
+		for event in pygame.event.get():
+			if event.type == KEYDOWN:
+				if event.key == K_UP:
+					# here is the Menu class method
+					menu.draw(-1)
+				if event.key == K_DOWN:
+					# here is the Menu class method
+					menu.draw(1)
+				if event.key == K_RETURN:
+					# here is the Menu class method
+					if menu.get_position() == 0:
+						game.start()
+					elif menu.get_position() == 1:
+						pygame.display.quit()
+						sys.exit()
+				if event.key == K_ESCAPE:
+					pygame.display.quit()
+					sys.exit()
+				pygame.display.update()
+			elif event.type == QUIT:
+				pygame.display.quit()
+				sys.exit()
+		pygame.time.wait(8)
 
-clock = pygame.time.Clock()
 
-done = False
-
-speed = 1 # Speed of the airplane
-tspeed = 3 # Turning speed
-tesla = ttesla = 0 # Time elapsed since last action
-player.changespeed(0,speed) # Not turning at t=0
-dt = clock.tick(FPS) # delta of t
-pos = POS_RIGHT # Start the game @ left position
-while not done:
-	screen.fill(WHITE) # Clean the screen
-	tesla += dt
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
-		elif event.type == EVENT_TL:
-			player.changespeed(-tspeed, speed)
-			pygame.time.set_timer(EVENT_TL, 0) # Stop the event to be repeated
-		elif event.type == EVENT_TR:
-			player.changespeed(tspeed, speed)
-			pygame.time.set_timer(EVENT_TR, 0) # Stop the event to be repeated
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				pygame.time.set_timer(EVENT_TL,250) #25 ms before the action is realised
-			elif event.key == pygame.K_RIGHT:
-				pygame.time.set_timer(EVENT_TR,250) #25 ms before the action is realised
-	if (tesla > 2000):
-		pos = 1-pos #Turn in the opposite dir.
-		tesla = 0 # Reset timer
-		gen_wall(pos) #Generate a wall
-		print "Time to generate wall"
-	# Rendering
-	all_sprite_list.draw(screen) # Draw everything so that text will be on top
-	all_sprite_list.update()
-	pygame.display.flip()
-	clock.tick(FPS) #Frame rate (in milliseconds)
-pygame.quit()
+def main():
+	show_menu({'Start':'game.start()', 'Quit':'sys.exit()'})
+	
+	#game.start()
+if __name__ == '__main__': main()
